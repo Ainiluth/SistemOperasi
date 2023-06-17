5bde714faefdf508f7a499f34513f23ac84a3677
Ainiluth <136144828+Ainiluth@users.noreply.github.com>
2023-06-17 09:24:52 +0700

    Update SO_latihan1.bat
    Hasil SO_latihan1.bat

--- SO_latihan1.bat
+++ SO_latihan1.bat
@@ -1,32 +1,31 @@
-@echo 0ff
-title latihan 5 perhitungan luas segitiga
+@echo off
+title latihan 5 Perhitungan Luas Segitiga
+rem ===================setting variabel=====================
+echo -------------------------------------------------------
+echo               INPUTAN ALAS DAN TINGGI 
+echo -------------------------------------------------------
+set /p alas= Masukkan Nilai Alas    :
+set /p tinggi= Masukkan Nilai Tinggi:
 
-rem ===========setting variabel============
-echo --------------------------------------
-echo     Inputan Nilai Alas dan Tinggi
-echo --------------------------------------
-set /p alas=Masukan Nilai Alas     =
-set /p tinggi=Masukan Nilai Tinggi =
-
-rem ==========setting perhitungan==========
+rem ==================setting perhitungan===================
 set /a luas=alas*tinggi/2
 
-if %luas% GEQ 500(
-   set grade=Kategori Besar
-) else if %luas% GEQ 100(
-   set grade=Kategori  Sedang
+if %luas% GEQ 500 (
+   set grade=Kategori Besar 
+) else if %luas% GEQ 100 (
+   set grade=Kategori Sedang
 ) else (
    set grade=Kategori Kecil
-)
-cls 
+) 
+cls
 
-rem ================output==================
-echo ---------------------------------------
-echo       Perhitungan Luas Segitiga
-echo ---------------------------------------
-echo Alas   : %alas%
-echo Tinggi : %tinggi%
-echo ---------------------------------------
-echo Luas segitiga tersebut adalah :%luas%
+rem ===================Output===============================
+echo -------------------------------------------------------
+echo             PERHITUNGAN LUAS SEGITIGA 
+echo -------------------------------------------------------
+echo Alas  : %alas%
+echo Tinggi: %tinggi%
+echo -------------------------------------------------------
+echo Luas Segitiganya adalah       :%luas%
 echo Kategori bangunannya adalah   :%grade%
-pause
+pause >nul
